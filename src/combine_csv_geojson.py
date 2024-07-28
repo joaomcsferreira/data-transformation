3854f24281d6a8a7e9ef6149483eccd45df680a8
joaomcsferreira <marcosc024@gmail.com>
2024-07-28 16:00:12 -0300

    add result folder

--- src/combine_csv_geojson.py
+++ src/combine_csv_geojson.py
@@ -1,49 +1,54 @@
 import json
 import pandas as pd
 
+
 # Função para carregar os dados JSON
 def carregar_json(arquivo_json):
-    with open(arquivo_json, 'r', encoding='utf-8') as f:
+    with open(arquivo_json, "r", encoding="utf-8") as f:
         dados_json = json.load(f)
     return dados_json
 
+
 # Função para carregar os dados CSV
 def carregar_csv(arquivo_csv):
-    dados_csv = pd.read_csv(arquivo_csv, sep=',', dtype=str)
+    dados_csv = pd.read_csv(arquivo_csv, sep=";", dtype=str)
     return dados_csv
+
 
 def isnumber(value):
     try:
-         float(value)
+        float(value)
     except ValueError:
-         return False
+        return False
     return True
+
 
 # Função para adicionar informações do CSV ao JSON
 def adicionar_info_json(dados_json, dados_csv):
     # Criar um dicionário de mapeamento a partir do CSV
-    mapeamento_csv = dados_csv.set_index('Cod').to_dict(orient='index')
+    mapeamento_csv = dados_csv.set_index("Cod").to_dict(orient="index")
 
     # Iterar sobre as features do JSON e adicionar informações do CSV
-    for feature in dados_json['features']:
-        cd_mun = feature['properties']['cd_mun']
+    for feature in dados_json["features"]:
+        cd_mun = feature["properties"]["cd_mun"]
         if cd_mun in mapeamento_csv:
             # Adicionar todas as informações do CSV ao JSON
             for chave, valor in mapeamento_csv[cd_mun].items():
-                feature['properties'][chave] = valor.strip()
-                
+                feature["properties"][chave] = valor.strip()
 
     return dados_json
 
+
 # Função para salvar o JSON atualizado
 def salvar_json(dados_json, arquivo_json):
-    with open(arquivo_json, 'w', encoding='utf-8') as f:
+    with open(arquivo_json, "w", encoding="utf-8") as f:
         json.dump(dados_json, f, ensure_ascii=False, indent=4)
 
+
 # Caminhos dos arquivos
-arquivo_json = 'csv/municipios.json'
-arquivo_csv = 'csv/data.csv'
-arquivo_json_atualizado = 'municipios.json'
+arquivo_json = "geojson/municipios.json"
+arquivo_csv = "csv/rural.csv"
+arquivo_json_atualizado = "rural.json"
 
 # Carregar dados
 dados_json = carregar_json(arquivo_json)
--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
-import pandas as pd
 import geojson
+import pandas as pd
+
 from math import isnan
+
 
 def csv_to_geojson(csv_file_path, geojson_file_path, lat_col, lon_col):
     # Ler o arquivo CSV
-    df = pd.read_csv(csv_file_path, delimiter=',')  # Adicionando delimitador correto
-    
+    df = pd.read_csv(csv_file_path, delimiter=",")  # Adicionando delimitador correto
+
     # Verificar se as colunas estão presentes no DataFrame
     if lat_col not in df.columns or lon_col not in df.columns:
-        raise ValueError(f"Colunas especificadas não encontradas no CSV. Colunas disponíveis: {df.columns}")
+        raise ValueError(
+            f"Colunas especificadas não encontradas no CSV. Colunas disponíveis: {df.columns}"
+        )
 
     # Lista para armazenar os pontos GeoJSON
     features = []
@@ -20,7 +24,7 @@ def csv_to_geojson(csv_file_path, geojson_file_path, lat_col, lon_col):
         lon = row[lon_col]
         lat = None if pd.isna(lat) else lat
         lon = None if pd.isna(lon) else lon
-        
+
         # Criar um objeto Point GeoJSON se ambos lat e lon são válidos
         if lat is not None and lon is not None:
             point = geojson.Point((lon, lat))
@@ -28,7 +32,9 @@ def csv_to_geojson(csv_file_path, geojson_file_path, lat_col, lon_col):
             point = None
 
         # Criar um objeto Feature GeoJSON
-        properties = {key: (None if pd.isna(value) else value) for key, value in row.items()}
+        properties = {
+            key: (None if pd.isna(value) else value) for key, value in row.items()
+        }
         feature = geojson.Feature(geometry=point, properties=properties)
 
         # Adicionar o objeto Feature à lista de features
@@ -38,13 +44,13 @@ def csv_to_geojson(csv_file_path, geojson_file_path, lat_col, lon_col):
     feature_collection = geojson.FeatureCollection(features)
 
     # Salvar o FeatureCollection em um arquivo GeoJSON
-    with open(geojson_file_path, 'w') as f:
+    with open(geojson_file_path, "w") as f:
         geojson.dump(feature_collection, f, indent=2)
 
 
-csv_file_path = 'csv/base_minerios_municipios.csv' 
-geojson_file_path = 'geojson/base_minerios_municipios.geojson'
-lat_col = 'lat'  
-lon_col = 'lon' 
+csv_file_path = "csv/base_minerios_municipios.csv"
+geojson_file_path = "geojson/base_minerios_municipios.geojson"
+lat_col = "lat"
+lon_col = "lon"
 
 csv_to_geojson(csv_file_path, geojson_file_path, lat_col, lon_col)
